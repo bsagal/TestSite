0c08e81e26996c6382859b18871a5b57f82e3220
Binyamin Sagal <bsagal@gmail.com>
2021-03-22 10:02:45 +0200

    Add Data project

---
+++ TestSite/App_Code/DbUtils.cs
@@ -0,0 +1,8 @@
+﻿using System.Data.Common;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+
+public static class DbUtils
+{
+    public static DbConnection CreateConnection() => new SqlConnection(WebConfigurationManager.ConnectionStrings["Data"].ConnectionString);
+}
